e74ea0433af7d0e3a73e964718e1b035949ab1fe
Corey Roberts <croberts22@utexas.edu>
2016-08-17 18:51:12 -0500

    Initial commit.

---
+++ SmashBracket/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SmashBracket
+//
+//  Created by Corey Roberts on 8/17/16.
+//  Copyright © 2016 Corey Roberts. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
